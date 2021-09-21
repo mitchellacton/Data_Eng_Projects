5b8f5f06723fab3335c5b34413a29b142783acd2
mitchacton <mitchellacton1@gmail.com>
2021-09-21 15:35:26 +0000

    automated host data table creation

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS host_info
+(
+    id SERIAL           PRIMARY KEY NOT NULL,
+    hostname            VARCHAR UNIQUE NOT NULL,
+    cpu_number          INT NOT NULL,
+    cpu_architecture    VARCHAR NOT NULL,
+    cpu_model           VARCHAR NOT NULL,
+    cpu_mhz             FLOAT NOT NULL,
+    l2_cache            INT NOT NULL,
+    total_mem           INT NOT NULL,
+    "timestamp"         TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS host_usage
+(
+    "timestamp"     TIMESTAMP NOT NULL,
+    host_id SERIAL  PRIMARY KEY NOT NULL,
+    memory_free     INT NOT NULL,
+    cpu_idle        FLOAT NOT NULL,
+    cpu_kernel      FLOAT NOT NULL,
+    disk_io         INT NOT NULL,
+    disk_available  INT NOT NULL,
+    CONSTRAINT fk_usage
+    FOREIGN KEY(host_id)
+    REFERENCES host_info(id)
+    )
